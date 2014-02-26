92287e3fbd25ea9e42dc65f352b68a8815137a47
David Estes <davydotcom@gmail.com>
2014-02-26 08:42:04 -0800

    bumping dev to 2.3.6

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,31 +1,18 @@
 grails.project.work.dir = 'target'
-grails.project.target.level = 1.6
-grails.project.source.level = 1.6
 
+grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
-
     inherits 'global'
     log 'warn'
-
+    legacyResolve false
     repositories {
         grailsCentral()
-        mavenLocal()
         mavenCentral()
     }
 
-    dependencies {
-        test 'org.spockframework:spock-grails-support:0.7-groovy-2.0', {
-            export = false
-        }
-    }
-
     plugins {
-        build ':release:2.2.1', ':rest-client-builder:1.0.3', {
-            export = false
-        }
-
-        test ':spock:0.7', {
-            excludes 'spock-grails-support', 'hibernate', 'grails-hibernate'
+        build ':tomcat:7.0.50.1'
+        build(':release:3.0.1') {
             export = false
         }
     }
