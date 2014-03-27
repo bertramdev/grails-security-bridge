c3704188866a510ffb4c5f71c8a4680e30e33809
Jeremy Michael Crosbie <jcrosbie@bertramcapital.com>
2014-03-27 13:38:37 -0600

    Version bump

--- SecurityBridgeGrailsPlugin.groovy
+++ SecurityBridgeGrailsPlugin.groovy
@@ -15,7 +15,7 @@
  */
 class SecurityBridgeGrailsPlugin {
     // the plugin version
-    def version = "0.5.3"
+    def version = "0.5.4"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion   = "2.0 > *"
 
