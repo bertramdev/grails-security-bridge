223d424b6caac254bc246786da6c3619dbb5482f
David Estes <davydotcom@gmail.com>
2014-06-05 14:14:48 -0400

    version bump

--- SecurityBridgeGrailsPlugin.groovy
+++ SecurityBridgeGrailsPlugin.groovy
@@ -15,7 +15,7 @@
  */
 class SecurityBridgeGrailsPlugin {
     // the plugin version
-    def version = "0.5.4"
+    def version = "0.6.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion   = "2.0 > *"
 
