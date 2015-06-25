2fb185d9e1a111bb0141b84e2f6fecfc2d0c0751
William Chu <will.chu@me.com>
2015-06-25 14:18:46 -0700

    Fixed bad formatting for developer attribution

--- SecurityBridgeGrailsPlugin.groovy
+++ SecurityBridgeGrailsPlugin.groovy
@@ -25,7 +25,8 @@ class SecurityBridgeGrailsPlugin {
     def description = 'Defines a standard cross-plugin security bridge implementation for better decoupling of authentication in plugin heavy applications.'
 
     def developers = [
-        [name: 'Jeremy Michael Crosbie', email: 'jcrosbie@bcap.com', name: 'William Chu', email: 'wchu@bcap.com']
+        [name: 'Jeremy Michael Crosbie', email: 'jcrosbie@bcap.com'],
+        [name: 'William Chu', email: 'wchu@bcap.com']
     ]
     def documentation   = "http://bertramdev.github.io/grails-security-bridge"
     def license = "APACHE"
