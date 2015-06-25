23e79bf70765ecf2750e1006911ad85e83255de1
David Estes <davydotcom@gmail.com>
2015-06-25 16:57:56 -0400

    version bump with namespace controller support

--- SecurityBridgeGrailsPlugin.groovy
+++ SecurityBridgeGrailsPlugin.groovy
@@ -15,7 +15,7 @@
  */
 class SecurityBridgeGrailsPlugin {
     // the plugin version
-    def version = "0.6.2"
+    def version = "0.7.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion   = "2.0 > *"
 
