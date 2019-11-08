0c9f4e223dcf11c3569ad3fd295d4cf890d77738
Eric Helgeson <eric@agileorbit.com>
2019-11-08 11:06:17 -0600

    Initial Grails 4 support. TODO: Publishing updates

--- src/test/groovy/org/grails/plugin/securitybridge/SecurityBridgeInterceptorSpec.groovy
+++ src/test/groovy/org/grails/plugin/securitybridge/SecurityBridgeInterceptorSpec.groovy
@@ -1,22 +1,9 @@
 package org.grails.plugin.securitybridge
 
-
-import grails.test.mixin.TestFor
+import grails.testing.web.interceptor.InterceptorUnitTest
 import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
- */
-@TestFor(SecurityBridgeInterceptor)
-class SecurityBridgeInterceptorSpec extends Specification {
-
-    def setup() {
-    }
-
-    def cleanup() {
-
-    }
-
+class SecurityBridgeInterceptorSpec extends Specification implements InterceptorUnitTest<SecurityBridgeInterceptor> {
     void "Test securityBridge interceptor matching"() {
         when:"A request matches the interceptor"
             withRequest(controller:"securityBridge")
